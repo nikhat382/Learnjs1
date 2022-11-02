1e1f6da5d46dd4083168cfe9e71733bc2fb37feb
nikhat382 <116967870+nikhat382@users.noreply.github.com>
2022-11-02 16:31:50 +0530

    Create Basics.js

---
+++ Basics.js
@@ -0,0 +1,29 @@
+console.log("my first code in javascript");
+var myname="Nikhat";
+var number=9876543;
+var myteckstack=["nodejs","react","mongo", "reactnative"];
+var myDetails={
+    firstname:"nikhat",
+    lastname:"naaz",
+};
+console.log(myname,number);
+console.log(myDetails);
+console.log(myteckstack);
+let collegename="dbit";
+let hometown="rohtas";
+let pincode=825301;
+console.log(collegename,hometown,pincode);
+//convert no into string//
+var myphone=9876554;
+console.log(typeof myphone);
+myphone=myphone.toString();
+console.log(typeof myphone);
+var pincode1="825301";
+console.log(typeof pincode1);
+pincode=parseInt(pincode1);
+console.log(pincode1);
+//convert decimal to integer//
+console.log(parseInt(12345.678));
+
+
+
